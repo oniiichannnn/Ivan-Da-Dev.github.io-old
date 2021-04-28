5201da3244178ae94f3c6197ac45734d55f7626e
Deus <67128188+Ivan-Da-Dev@users.noreply.github.com>
2021-04-28 21:32:39 +0800

    Add files via upload

--- reviews/script.js
+++ reviews/script.js
@@ -208,7 +208,6 @@ const emojis = [
 const linkReg = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g
 
 function load_animes(){
-
     let loadFromURL = false
 
     if(
