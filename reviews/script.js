d8a401af8929f92e6ca898b6ad1213a1683c8df8
Deus <67128188+Ivan-Da-Dev@users.noreply.github.com>
2021-05-05 11:21:37 +0800

    Add files via upload

--- reviews/script.js
+++ reviews/script.js
@@ -432,8 +432,8 @@ function display_anime(anime){
     document.getElementById('comments_script').text =
     `
      var disqus_config = function () {
-     this.page.url = "${document.URL}";  // Replace PAGE_URL with your page's canonical URL variable
-     this.page.identifier = "${anime.title.replace(/ /g,'_')}"; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+     this.page.url = "${document.URL.replace(/\&scroll=[0-9]+/,'')}";  // Replace PAGE_URL with your page's canonical URL variable
+     this.page.identifier = "${encodeURL(anime.title)}"; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
      };
 
      (function() { // DON'T EDIT BELOW THIS LINE
